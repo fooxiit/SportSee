3229315d211060bbc83e0ac25fa9bea9a05fc2d7
guillian <guillian.edo@gmail.com>
2025-12-29 18:11:44 +0100

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1>sport see</h1>
+        </>
+    );
+}
+
+export default App;
---
+++ src/constante/String.js
@@ -0,0 +1,44 @@
+const STRING = {
+    FR: {
+        CATCHPHRASE: 'Félicitation ! Vous avez explosé vos objectifs hier 👏',
+        LINK: {
+            HOME: 'Accueil',
+            PROFIL: 'Profil',
+            SETING: 'Réglage',
+            COMUNITY: 'Communauté',
+        },
+        CHARTS: {
+            DAILY: {
+                TITTLE: 'Activité quotidienne',
+                LEDGEND: {
+                    WEIGHT: 'Poids (kg)',
+                    BURN: 'Calories brûlées (kCal)',
+                },
+            },
+            SESION_TIME: {
+                TITLE: 'Durée moyenne des sessions',
+            },
+            STATS: {
+                LEDGEND: {
+                    INTENSITY: 'Intensité',
+                    SPEED: 'Vitesse',
+                    STRENGTH: 'Force',
+                    ENDURANCE: 'Endurance',
+                    ENERGY: 'Energie',
+                    CARDIO: 'Cardio',
+                },
+            },
+            SCORE: {
+                TITLE: 'Score',
+                LEDGEND: 'de votre objectif',
+            },
+        },
+        INFO: {
+            CAL: 'Calories',
+            PROT: 'Proteines',
+            GLUC: 'Glucides',
+            LIP: 'Lipides',
+        },
+        COPIRIGHT: 'Copiryght, SportSee 2020',
+    },
+};
