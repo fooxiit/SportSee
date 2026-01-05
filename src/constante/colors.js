ca5a25a2fc370fd2f8ea6e48291faaec14b7c599
guillian <guillian.edo@gmail.com>
2026-01-05 03:53:05 +0100

    add const for charts color

---
+++ src/constante/colors.js
@@ -0,0 +1,32 @@
+const COLORS = {
+    CHART: {
+        DAILY: {
+            over: '#C4C4C480',
+            FILL: {
+                WEIGHT: '#E60000',
+                BURN: '#282D30',
+            },
+        },
+        SESION_TIME: {
+            FILL: {
+                TIME: '#FFFFFF',
+            },
+            BACKGROUND: '#FF0000',
+        },
+        STATS: {
+            FILL: {
+                ALL: '#FF0101B2',
+            },
+            BACKGROUND: '#282D30',
+            CHART_LINE: '#FFFFFF',
+        },
+        SCORE: {
+            BACKGROUND: '#FBFBFB',
+            FILL: {
+                SCORE: '#E60000',
+            },
+        },
+    },
+};
+
+export default COLORS;
