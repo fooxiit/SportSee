04208054c04a55fce7e592f7c5bf22ef001334db
guillian <guillian.edo@gmail.com>
2026-01-07 09:22:32 +0100

    add links constant

---
+++ src/constante/link.js
@@ -0,0 +1,17 @@
+import STRING from './String';
+
+const LINKS = [
+    { name: STRING.FR.LINK.HOME, href: '#' },
+    { name: STRING.FR.LINK.PROFIL, href: '#' },
+    { name: STRING.FR.LINK.SETING, href: '#' },
+    { name: STRING.FR.LINK.COMUNITY, href: '#' },
+];
+
+export const NAV_ICONS = [
+    { imgSrc: '/yoga.svg', alt: 'yoga' },
+    { imgSrc: '/swimming.svg', alt: 'swimming' },
+    { imgSrc: '/cycle.svg', alt: 'cycle' },
+    { imgSrc: '/gym.svg', alt: 'gym' },
+];
+
+export default LINKS;
